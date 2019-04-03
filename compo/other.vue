05d93da6cb46f8b02bd41e21f52192cb7a51cfa1
garebareDA <garedaiki@outlook.jp>
2019-04-03 12:27:36 +0000

    Subtitleの追加

--- compo/other.vue
+++ compo/other.vue
@@ -25,6 +25,25 @@
       <p class="con">Unity,C#,Node.jsで作りました</p>
       <p>スマートフォンの傾きと連動し首が動き</p>
       <p>リップシンクで口が動きます</p>
+
+      <hr>
+
+      <p class="t">Subtitles--Generator</p>
+      <video controls  webkit-playsinline playsinline autoplay loop muted name="media" class="jsImg sizeImg">
+        <source src="./otherImg/2019-04-03_20-19-30.mp4" type="video/mp4">
+      </video>
+      <br>
+
+      <a
+        href="https://github.com/garebareDA/Subtitles--Generator"
+        target="_blank"
+        class="button is-dark is-inverted is-outlined"
+      >GitHub</a>
+      <p class="con">Node.jsで作成しました</p>
+      <p>node.jsでサーバを立ち上げ</p>
+      <p>hromeでlocalhost:8000にアクセスすれば</p>
+      <p>マイクの音声から字幕が生成されます</p>
+
     </div>
   </div>
 </template>
