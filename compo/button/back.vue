694038a385874acf85e074f35b49c366ed65c05a
garebareDA <garedaiki@gmail.com>
2020-10-31 05:53:11 +0000

    add:Unity and git

--- compo/button/back.vue
+++ compo/button/back.vue
@@ -5,7 +5,5 @@
         <router-link to="/" class="button is-large is-link">戻る</router-link>
       </div>
     </div>
-
-    <img class="head" src="../img/charcter/head.gif">
   </div>
 </template>
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const { VueLoaderPlugin } = require('vue-loader');
 const path = require('path');
 
 module.exports = {
-  mode:'development',
+  mode:'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
