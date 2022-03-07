2f37c4f3ff1bafe6c8c31054b988e4ec35fdb727
garebareDA <garedaiki@gmail.com>
2022-03-07 16:41:32 +0900

    add:handling

--- app/root.tsx
+++ app/root.tsx
@@ -5,9 +5,14 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
+  useNavigate
 } from "remix";
 import type { MetaFunction, LinksFunction } from "remix";
 import { NextUIProvider } from "@nextui-org/react";
+import { Container, Text, Card, Row, Spacer, Button } from "@nextui-org/react";
+import React, { CSSProperties } from "react";
+
 export const meta: MetaFunction = () => {
   return { title: "Garebare's Portfolio" };
 };
@@ -16,9 +21,13 @@ export const links: LinksFunction = () => {
   return [{ rel: "shortcut icon", href: "https://garebareda.github.io/portfolio/app/assets/images/icon/garebare-pixel.png" }];
 };
 
+const backgroundColor:CSSProperties = {
+  backgroundColor: "#003e80"
+};
+
 export default function App() {
   return (
-    <html lang="ja" style={{ backgroundColor: "#003e80" }}>
+    <html lang="ja" style={backgroundColor}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
@@ -32,10 +41,11 @@ export default function App() {
         <meta name="twitter:title" content="garebare's portfolio" />
         <meta name="twitter:site" content="https://www.garebare.net" />
         <meta name="twitter:image" content="https://www.garebareda.github.io/portfolio/app/assets/images/Thumbnail.webp" />
+
         <Meta />
         <Links />
       </head>
-      <body style={{ backgroundColor: "#003e80" }}>
+      <body style={backgroundColor}>
         <NextUIProvider>
           <Outlet />
           <ScrollRestoration />
@@ -46,3 +56,56 @@ export default function App() {
     </html>
   );
 }
+
+export function CatchBoundary(): React.ReactElement {
+  const caught = useCatch();
+  return (
+    <html lang="ja" style={backgroundColor}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta property="og:title" content="garebare's portfolio" />
+        <meta property="og:site_name" content="garebare's portfolio" />
+        <meta property="og:url" content="https://www.garebare.net" />
+        <meta property="og:description" content="ガレバレのポートフォリオです" />
+        <meta property="og:image" content="https://garebareda.github.io/portfolio/app/assets/images/Thumbnail.webp" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="garebare's portfolio" />
+        <meta name="twitter:site" content="https://www.garebare.net" />
+        <meta name="twitter:image" content="https://www.garebareda.github.io/portfolio/app/assets/images/Thumbnail.webp" />
+
+        <Meta />
+        <Links />
+        <title> {caught.status}  </title>
+      </head>
+      <body style={backgroundColor}>
+        <NextUIProvider>
+          <Container justify="center">
+            <Spacer y={3} />
+            <Row justify="center">
+              <Card css={{
+                mw: "50%",
+              }}>
+                <Row justify="center">
+                  <Text size={40}>{caught.status}</Text>
+                </Row>
+                <Row justify="center">
+                  <Text size={32}>{caught.statusText}</Text>
+                </Row>
+                <Spacer y={3} />
+                <Row justify="center">
+                    <Button onClick={() => {
+                      location.href = "/";
+                    }}>メインページに戻る</Button>
+                </Row>
+              </Card>
+            </Row>
+          </Container>
+          <Scripts />
+          <LiveReload />
+        </NextUIProvider>
+      </body>
+    </html>
+  );
+};
