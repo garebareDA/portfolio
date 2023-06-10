a1a5eca0a8b9d204b104e44914be3ab034813cce
garebareDA <garedaiki@gmail.com>
2023-06-11 01:55:31 +0900

    fix:resume

--- app/components/index/resume.tsx
+++ app/components/index/resume.tsx
@@ -1,4 +1,4 @@
-import { Card, Text, Link } from "@nextui-org/react"
+import { Card, Text, Link, Row } from "@nextui-org/react"
 
 export function Resume() {
   return (
@@ -12,13 +12,15 @@ export function Resume() {
         </Card.Header>
 
         <Card.Body>
-          <Link
-            href="https://garebare.notion.site/garebare/3c37ea98234f4c8cba7b95ff2f8ae2b4"
-            icon
-            target="_blank"
-            rel="noopener noreferrer">
-            Notion
-          </Link>
+          <Row>
+            <Link
+              href="https://garebare.notion.site/garebare/3c37ea98234f4c8cba7b95ff2f8ae2b4"
+              icon
+              target="_blank"
+              rel="noopener noreferrer">
+              Notion
+            </Link>
+          </Row>
         </Card.Body>
       </Card>
     </>
